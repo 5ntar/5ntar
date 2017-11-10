9dbeef71399579f64db47cfc9f0ac892303ad20a
lulitamartin <lulitamartin@hotmail.com>
2017-11-10 18:36:48 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function Denunciar(){
+					
+	href="https://lulitamartin.github.io/5ntar/5ntarformulario.html";
+}
+<div id= "CARTO.JS" cartodb.createVis('map', hhttps://carocet.carto.com/builder/469e92ce-e4c2-4052-b4a9-99bbbd33ae52/embedq=INSERT INTO table_5ntar map 1(tipo,gravedad,hora,lugar,descripcion,lat,long)VALUES (‘caro’, ‘5/4/2017’, ‘Sucre 3073’, ‘-34.44’, ‘-58.88’) a3a2ddddf9f466f32b683a0257efe3b04480f886); 
+								    cartodb.createVis('map', 'https://carocet.carto.com/builder/469e92ce-e4c2-4052-b4a9-99bbbd33ae52/embedq=INSERT INTO table_5ntar map 1(tipo,gravedad,hora,lugar,descripcion,lat,long)VALUES (‘caro’, ‘5/4/2017’, ‘Sucre 3073’, ‘-34.44’, ‘-58.88’) a3a2ddddf9f466f32b683a0257efe3b04480f886) {
+								    layers[1].setInteraction(true);
+    								layers[1].on('featureOver', function(e, latlng, pos, data, layerNumber) {
+     								 console.log(e, latlng, pos, data, layerNumber);
+   									 });
+   									  var map = vis.getNativeMap();
+   									 </div>
