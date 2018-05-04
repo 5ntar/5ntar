ba731833c72f14b196e56bb05a91794f3859cefb
lulitamartin <lulitamartin@hotmail.com>
2018-05-04 12:12:23 -0300

    Modifico funcion envio a carto

--- 5ntar.js
+++ 5ntar.js
@@ -11,13 +11,12 @@ function Saludar_a_confirmar(nombre, apellido){
 }
 
 
-function RevisarInformacion(nombre, apellido,tipoDeAcoso,edad,descripcion){
+function EnviarInformacionACarto(nombre, lugar, hora, edad, tipo, descripcion){
+	var valores = "%27" +nombre+"%27,%20%27"+lugar+"%27,%20%27"+hora+"%27,%20"+edad+",%20%27"+tipo+"%27,%20%27"+descripcion+"%27";
 
-	var nombreCompleto= nombre + " " + apellido;
-	var confirmar = "El nombre completo es: " + nombreCompleto 
-					+ "\nLa edad es " + edad 
-					+ "\nEl mail es: " +  tipoDeAcoso
-					+ "\nEl mensaje es: " + descripcion;
+	var url ="https://lulitaamartin.carto.com/api/v2/sql?q=INSERT%20INTO%20table_5ntar%20(nombre,%20lugar,%20hora,%20edad,%20tipo,%20descripcion)%20VALUES%20("+ valores + ")&api_key=1WPttJtAhT1jXlrblvE1hg"
 
-	alert(confirmar);
+	alert(url);
+
 }
+
