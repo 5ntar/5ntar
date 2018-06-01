9e12a6722bd4cebbd7f934818b1871853467c46a
lulitamartin <lulitamartin@hotmail.com>
2018-06-01 09:24:19 -0300

    Update 5ntar.js

--- 5ntar.js
+++ 5ntar.js
@@ -2,7 +2,7 @@
 
 
 function EnviarInformacionACarto(nombre, lugar, hora, edad, tipo, descripcion){
-	var valores = "%27" +nombre+"%27,%20%27"+lugar+"%27,%20%27"+hora+"%27,%20%27"+edad+"%27,%20%27"+tipo+" +descripcion+"%27";
+	var valores = "%27" +nombre+"%27,%20%27"+lugar+"%27,%20%27"+hora+"%27,%20%27"+edad+"%27,%20%27"+tipo+"%27,%20%27"+descripcion+"%27";
 	var url ="https://lulitaamartin.carto.com/api/v2/sql?q=INSERT%20INTO%20table_5ntar%20(nombre,lugar,hora,edad,tipo,descripcion)VALUES("+ valores +")&api_key=1WPttJtAhT1jXlrblvE1hg"
 	//var res = encodeURI(url);
         // document.getElementById("valores").innerHTML = res;
